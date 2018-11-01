950cafc63ec837235ecaedad84c1760a1303af9e
Vlad8161 <vlad81611@yandex.ru>
2018-11-02 00:43:11 +0300

    distribute workers

--- main.py
+++ main.py
@@ -15,75 +15,22 @@ class Hydralisk(sc2.BotAI):
         return self.enemy_start_locations[0]
 
     async def on_step(self, iteration):
-        larvae = self.units(UnitTypeId.LARVA)
-        forces = self.units(UnitTypeId.ZERGLING) | self.units(UnitTypeId.HYDRALISK)
+        if iteration == 0:
+            townhall = self.townhalls.first if self.townhalls.exists else None
+            drones = self.units(UnitTypeId.DRONE).closer_than(10.0, townhall)
+            for drone in drones:
+                await self.do(drone.hold_position())
 
-        if self.units(UnitTypeId.HYDRALISK).amount > 10 and iteration % 50 == 0:
-            for unit in forces.idle:
-                await self.do(unit.attack(self.select_target()))
-
-        if self.supply_left < 2:
-            if self.can_afford(UnitTypeId.OVERLORD) and larvae.exists:
-                await self.do(larvae.random.train(UnitTypeId.OVERLORD))
-                return
-
-        if self.units(UnitTypeId.HYDRALISKDEN).ready.exists:
-            if self.can_afford(UnitTypeId.HYDRALISK) and larvae.exists:
-                await self.do(larvae.random.train(UnitTypeId.HYDRALISK))
-                return
-
-        if not self.townhalls.exists:
-            for unit in self.units(UnitTypeId.DRONE) | self.units(UnitTypeId.QUEEN) | forces:
-                await self.do(unit.attack(self.enemy_start_locations[0]))
-            return
-        else:
-            hq = self.townhalls.first
-
-        for queen in self.units(UnitTypeId.QUEEN).idle:
-            abilities = await self.get_available_abilities(queen)
-            if AbilityId.EFFECT_INJECTLARVA in abilities:
-                await self.do(queen(UnitTypeId.EFFECT_INJECTLARVA, hq))
-
-        if not (self.units(UnitTypeId.SPAWNINGPOOL).exists or self.already_pending(UnitTypeId.SPAWNINGPOOL)):
-            if self.can_afford(UnitTypeId.SPAWNINGPOOL):
-                await self.build(UnitTypeId.SPAWNINGPOOL, near=hq.position)
-
-        if self.units(UnitTypeId.SPAWNINGPOOL).ready.exists:
-            if not self.units(UnitTypeId.LAIR).exists and hq.noqueue:
-                if self.can_afford(UnitTypeId.LAIR):
-                    await self.do(hq.build(UnitTypeId.LAIR))
-
-        if self.units(UnitTypeId.LAIR).ready.exists:
-            if not (self.units(UnitTypeId.HYDRALISKDEN).exists or self.already_pending(UnitTypeId.HYDRALISKDEN)):
-                if self.can_afford(UnitTypeId.HYDRALISKDEN):
-                    await self.build(UnitTypeId.HYDRALISKDEN, near=hq.position)
-
-        if self.units(UnitTypeId.EXTRACTOR).amount < 2 and not self.already_pending(UnitTypeId.EXTRACTOR):
-            if self.can_afford(UnitTypeId.EXTRACTOR):
-                drone = self.workers.random
-                target = self.state.vespene_geyser.closest_to(drone.position)
-                err = await self.do(drone.build(UnitTypeId.EXTRACTOR, target))
-
-        if hq.assigned_harvesters < hq.ideal_harvesters:
-            if self.can_afford(UnitTypeId.DRONE) and larvae.exists:
-                larva = larvae.random
-                await self.do(larva.train(UnitTypeId.DRONE))
-                return
-
-        for a in self.units(UnitTypeId.EXTRACTOR):
-            if a.assigned_harvesters < a.ideal_harvesters:
-                w = self.workers.closer_than(20, a)
-                if w.exists:
-                    await self.do(w.random.gather(a))
-
-        if self.units(UnitTypeId.SPAWNINGPOOL).ready.exists:
-            if not self.units(UnitTypeId.QUEEN).exists and hq.is_ready and hq.noqueue:
-                if self.can_afford(UnitTypeId.QUEEN):
-                    await self.do(hq.train(UnitTypeId.QUEEN))
-
-        if self.units(UnitTypeId.ZERGLING).amount < 20 and self.minerals > 1000:
-            if larvae.exists and self.can_afford(UnitTypeId.ZERGLING):
-                await self.do(larvae.random.train(UnitTypeId.ZERGLING))
+            nearby_minerals = self.state.mineral_field.closer_than(10.0, townhall)
+            sc2.logger.info(nearby_minerals)
+            drones = self.units(UnitTypeId.DRONE).closer_than(10.0, townhall)
+            for mineral in nearby_minerals:
+                sc2.logger.info("{0}, {1}, {2}".format(mineral, mineral.assigned_harvesters, mineral.ideal_harvesters))
+                for i in range(2):
+                    if drones.exists:
+                        closest_drone = drones.closest_to(mineral)
+                        drones.remove(closest_drone)
+                        await self.do(closest_drone.gather(mineral))
 
 
 def main():
